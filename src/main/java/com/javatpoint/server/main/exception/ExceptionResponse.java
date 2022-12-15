a51135ad1cf320846956353a40cffedca5eab2fb
Vikas Gahlaut <gahlaut@adobe.com>
2022-12-15 16:06:34 +0530

    Add exception handling

---
+++ src/main/java/com/javatpoint/server/main/exception/ExceptionResponse.java
@@ -0,0 +1,31 @@
+package com.javatpoint.server.main.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	private Date timestamp;
+	private String message;
+	private String details;
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public String getDetails() {
+		return details;
+	}
+	
+	
+	
+}
--- src/main/java/com/javatpoint/server/main/user/UserResource.java
+++ src/main/java/com/javatpoint/server/main/user/UserResource.java
@@ -27,7 +27,10 @@ return service.findAll();
 @GetMapping("/users/{id}")  
 public User retriveUser(@PathVariable int id)  
 {  
-return service.findOne(id);  
+	User user = service.findOne(id);
+	if(user == null)
+		throw new UserNotFoundException("id:"+ id);
+return user;  
 }  
 
 //method that posts a new user detail   
